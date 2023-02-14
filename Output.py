9f07c4b8738cf2810e4c3bc879d7255932e07188
Jitendra Sanjay Bhavsar <124669007+Jitendra-Bhavsar@users.noreply.github.com>
2023-02-14 09:40:36 +0530

    Update Output.py

--- Output.py
+++ Output.py
@@ -22,5 +22,5 @@ placed order(costumer_order, costumer_detail), update product catalogue in mysql
   ii) order total price must be greater then 99 and less than 4999 (price_validation_3 module).
   iii) order items must be available in shop (product_available_2 module) and order quantity must be less or equal as compare to item_availabily   
 4) if code is bug then go to mysql database and truncate all tables and then run.
-5) if you have not see availableQuantity and price in update product catalogue then you need to run update_product_model to see update product catalogue. 
+5) if you have not see availableQuantity and price in update product catalogue then you need to run update_table module to see update product catalogue. 
 """
