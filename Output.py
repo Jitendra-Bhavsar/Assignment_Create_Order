df0c7b81060b45c6d67d19b13db76784898d264a
Jitendra Sanjay Bhavsar <124669007+Jitendra-Bhavsar@users.noreply.github.com>
2023-02-14 09:38:11 +0530

    Update Output.py

--- Output.py
+++ Output.py
@@ -11,7 +11,7 @@ obj = My_Output()
 # print the list of placed order.
 print("Final_Placed_Order_List: ",pd.DataFrame(obj.my_output()))
 # To see update product catalogue
-print("Update Product_catalogue: ",pd.DataFrame(update_function()))
+# print("Update Product_catalogue: ",pd.DataFrame(update_function()))
 
 """
 1) When you run this output module You get list of
@@ -22,5 +22,5 @@ placed order(costumer_order, costumer_detail), update product catalogue in mysql
   ii) order total price must be greater then 99 and less than 4999 (price_validation_3 module).
   iii) order items must be available in shop (product_available_2 module) and order quantity must be less or equal as compare to item_availabily   
 4) if code is bug then go to mysql database and truncate all tables and then run.
-5) if you not see availableQuantity and price in update product catalogue then you need to see in database mysql you will get. 
+5) if you have not see availableQuantity and price in update product catalogue then you need to run update_product_model to see update product catalogue. 
 """
